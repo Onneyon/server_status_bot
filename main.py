f802a152b1ade409506aff6f4221155d2956bbe4
Alex Coiffait <alexcoiffait@hotmail.co.uk>
2023-11-16 09:24:09 +0000

    implemented add command, yaml config

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import tasks, commands
 import urllib.request
 import json
+from yaml import safe_load, dump
 
 from discord.flags import Intents
 
@@ -10,20 +11,42 @@ intents.message_content = True
 
 
 class myBot(discord.ext.commands.Bot):
-    def __init__(self, command_prefix, api_url, channel_id, *, intents, **options):
-        super().__init__(command_prefix, intents=intents, **options)
-        self.api_url = api_url
-        self.channel_id = channel_id
-        self.ready = False
-
     async def setup_hook(self) -> None:
+        self.ready = False
+        self.servers = {}
+
+        self.load_config()
         update_status.start()
 
+    def load_config(self):
+        with open("servers.yaml", "r") as f:
+            self.servers = safe_load(f)
+            print(self.servers)
 
-bot = myBot("s!", intents=intents, api_url="https://api.battlemetrics.com/servers/16023402",
-            channel_id=1003246498751983676)
+    def save_config(self):
+        with open("servers.yaml", "w") as f:
+            dump(self.servers, f)
+
+    def add_status(self, api_url, channel_str):
+        self.servers[channel_str] = api_url
+        self.save_config()
+
+
+bot = myBot("s!", intents=intents)
 with open("token.txt", "r") as f:
     token = f.readline()
+
+
+@bot.command()
+async def add(ctx, api_url):
+    channel_str = str(ctx.message.channel.id)
+    bot.add_status(api_url, channel_str)
+
+
+@add.error
+async def info_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send('Error: bad argument')
 
 
 @bot.event
@@ -32,18 +55,25 @@ async def on_ready():
     bot.ready = True
 
 
-@tasks.loop(minutes=5.0)
+@tasks.loop(seconds=5.0)
 async def update_status():
     if not bot.ready:
         return
 
-    page = urllib.request.urlopen(bot.api_url)
-    json_return = json.loads(page.read().decode("utf-8"))
-    player_count = json_return["data"]["attributes"]["players"]
+    for channel_str in bot.servers:
+        api_url = bot.servers[channel_str]
 
-    channel = bot.get_channel(bot.channel_id)
-    if channel:
-        await channel.edit(topic=f"Players online: {player_count}")
+        try:
+            page = urllib.request.urlopen(api_url)
+            json_return = json.loads(page.read().decode("utf-8"))
+            player_count = json_return["data"]["attributes"]["players"]
+        except Exception:
+            continue
+
+        channel = bot.get_channel(int(channel_str))
+        if channel:
+            # await channel.edit(topic=f"Players online: {player_count}")
+            print(f"{player_count} players in {channel_str} at {api_url}")
 
 try:
     bot.run(token)
